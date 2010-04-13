983d64d5ca53bb3a4288c45734a57b590d88833b
A1ex <popescu.m.alexandru@gmail.com>
2010-04-13 22:27:57 +0300

    added left panel to wizard interface

---
+++ playrsa1.1/src/playrsa/Main.java
@@ -0,0 +1,11 @@
+
+package playrsa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RSAWizard rsaWizard = new RSAWizard();
+        rsaWizard.setVisible(true);
+    }
+
+}
